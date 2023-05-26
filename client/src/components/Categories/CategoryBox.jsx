478648d81257b19bf9b44e3aa5decac8f3322957
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-05-26 20:38:24 +0600

    first commit

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import qs from 'query-string'
 
 import { useNavigate, useSearchParams } from 'react-router-dom'
-const CategoryBox = ({ label, icon: Icon }) => {
+const CategoryBox = ({ label, icon: Icon, selected }) => {
   const [params, setParams] = useSearchParams()
 
   const navigate = useNavigate()
@@ -30,7 +30,24 @@ const CategoryBox = ({ label, icon: Icon }) => {
   return (
     <div
       onClick={handleClick}
-      className='flex cursor-pointer flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500'
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${
+          selected
+            ? 'border-b-neutral-800 text-neutral-800'
+            : 'border-transparent text-neutral-500'
+        }
+     
+      `}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
